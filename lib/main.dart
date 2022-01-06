a49f99614cd15b69ad772ecb0bdf97ede669884b
waleed <kurshany.waleed@gmail.com>
2022-01-06 12:11:47 +0100

    app inti

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Weather App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    //! UI of the application !
+    return Scaffold(
+      backgroundColor: const Color(0xFFf9f9f9),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFf9f9f9),
+        elevation: 0.0,
+        centerTitle: true,
+        title: const Text("Weather App", style: TextStyle(color: Colors.black)),
+      ),
+      body: Container(),
+    );
+  }
+}
